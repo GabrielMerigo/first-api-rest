61058c73183ef94263ef7e2ecd33a3384a10b0c2
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-02-12 01:46:00 -0300

    PUT realizado

--- index.js
+++ index.js
@@ -9,30 +9,97 @@ const DB = {
   games: [
     {
       name: 'GTA V',
-      id: 2312,
+      id: 1,
       year: 2013,
       price: 100
     },
     {
       name: 'Shadow of the colossus',
-
-      id: 8231,
+      id: 2,
       year: 2009,
       price: 50
     },
     {
       name: 'Dying Light',
-      id: 9374,
+      id: 3,
       year: 2016,
       price: 40
     },
   ]
 };
 
+const getGame = id => DB.games.find(game => game.id === id);
 
 app.get('/games', (req, res) => {
   res.statusCode = 200;
   res.json(DB.games)
+})
+
+app.get('/game/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  if (isNaN(id)) {
+    res.sendStatus(400)
+  } else {
+    const game = getGame(id);
+
+    if (!game) {
+      res.sendStatus(404)
+    } else {
+      res.json(game)
+    }
+  }
+
+});
+
+app.post('/game', (req, res) => {
+  const { name, year, price } = req.body;
+
+  if (name && year && price) {
+    DB.games.push({
+      id: 4,
+      name,
+      year,
+      price
+    })
+
+    res.sendStatus(200)
+  }else{
+    res.sendStatus(400)
+  }
+});
+
+app.delete('/game/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  if(isNaN(id)){
+    res.sendStatus(400)
+  }else{
+    const newGames = DB.games.filter(game => game.id !== id);
+    DB.games = newGames;
+    res.sendStatus(200)
+  }
+});
+
+app.put('/game/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (isNaN(id)) {
+    res.sendStatus(400)
+  } else {
+    const game = getGame(id);
+    const { name, year, price } = req.body;
+
+    if (!game) {
+      res.sendStatus(404)
+    } else {  
+      if (name) game.name = name;
+      if (year) game.year = year;
+      if (price) game.price = price;
+
+      res.sendStatus(200)
+    }
+  }
+
+
 })
 
 app.listen(8080, () => {
