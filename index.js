67371e2e6238bf5adf26ba492fad0f3f3aae81d8
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-02-18 21:53:27 -0300

    criando api de autenticação

--- index.js
+++ index.js
@@ -27,6 +27,20 @@ const DB = {
       year: 2016,
       price: 40
     },
+  ],
+  users: [
+    {
+      id: 1,
+      name: 'Gabriel',
+      email: 'gabismerigo@gmail.com',
+      password: '123456'
+    },
+    {
+      id: 1,
+      name: 'Izadora',
+      email: 'Izadora@gmail.com',
+      password: 'Izadora1234'
+    }
   ]
 };
 
@@ -65,16 +79,16 @@ app.post('/game', (req, res) => {
     })
 
     res.sendStatus(200)
-  }else{
+  } else {
     res.sendStatus(400)
   }
 });
 
 app.delete('/game/:id', (req, res) => {
   const id = parseInt(req.params.id);
-  if(isNaN(id)){
+  if (isNaN(id)) {
     res.sendStatus(400)
-  }else{
+  } else {
     const newGames = DB.games.filter(game => game.id !== id);
     DB.games = newGames;
     res.sendStatus(200)
@@ -92,13 +106,35 @@ app.put('/game/:id', (req, res) => {
 
     if (!game) {
       res.sendStatus(404)
-    } else {  
+    } else {
       if (name) game.name = name;
       if (year) game.year = year;
       if (price) game.price = price;
 
       res.sendStatus(200)
     }
+  }
+})
+
+app.post('/auth', (req, res) => {
+  const { email, password } = req.body;
+
+  if(!email || !password) return res.statusCode(400), res.json({ err: 'E-mail inválido' })
+
+  const user = DB.users.find(user => user.email === email);
+
+  if(!user){
+    if(user.password === password){
+      res.status = 200;
+      res.json({ token: "Token Falso!" });
+    }else{
+      res.status = 401; // Não autorizado
+      res.json({ err: "Credenciais inválidas!" });
+    }
+  
+  }else{
+    res.status = 404;
+    res.json({ err: 'O E-mail enviado não existe na base de dados.' });
   }
 })
 
